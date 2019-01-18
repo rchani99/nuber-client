d1842925a8f5aca1a12aaf784051cb57e43ccedb
rchani <rchani@uangel.com>
2019-01-18 17:26:38 +0900

    Apollo Setting 2

---
+++ src/Components/App/AppPresenter.tsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+import React from "react";
+// import styled from "../../typed-components";
+
+interface IProps {
+  isLoggedIn: boolean;
+}
+
+const AppPresenter: React.SFC<IProps> = ({ isLoggedIn }) =>
+  isLoggedIn ? <span>you are in</span> : <span>you are out</span>;
+
+AppPresenter.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired
+};
+
+export default AppPresenter;
