85a1a4950c8cf6b8cb0d55085d572559c1315399
rchani <rchani@uangel.com>
2019-03-06 18:00:05 +0900

    Planning the Routers

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # nuber-client
+
 Client for the (N)Uber Clone Course on Nomad Academy. ReactJS, Apollo, Typescript
+
+## Screen:
+
+### Logged Out:
+
+    - [ ] Home
+    - [ ] Phone Login
+    - [ ] Verify Phone Number
+    - [ ] Social Login
+
+### Logged In:
+
+    - [ ] Home
+    - [ ] Ride
+    - [ ] Edit Account
+    - [ ] Settings
+    - [ ] Saved Places
+    - [ ] Add Place
+    - [ ] Challenge : Ride History
--- src/Components/App/AppPresenter.tsx
+++ src/Components/App/AppPresenter.tsx
@@ -1,6 +1,15 @@
 import PropTypes from "prop-types";
 import React from "react";
-// import styled from "../../typed-components";
+
+import AddPlace from "../../Routes/AddPlace";
+import EditAccount from "../../Routes/EditAccount";
+import Home from "../../Routes/Home";
+import OutHome from "../../Routes/OutHome";
+import PhoneLogin from "../../Routes/PhoneLogin";
+import Places from "../../Routes/Places";
+import Ride from "../../Routes/Ride";
+import Settings from "../../Routes/Settings";
+import VerifyPhone from "../../Routes/VerifyPhone";
 
 interface IProps {
   isLoggedIn: boolean;
