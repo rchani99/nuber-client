556c6a7e196cd9b7418f3ef112af4fc7a1ad4791
rchani <rchani@uangel.com>
2019-03-08 17:33:58 +0900

    Finish Route Design

---
+++ src/Routes/OutHome/OutHomePresenter.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import styled from '../../typed-components';
+
+// tslint:disable-next-line:no-empty-interface
+interface IProps extends RouteComponentProps {
+}
+
+// RouteComponentProps<Param에 대한 타입 인터페이스> : { location, history, match, staticContext }
+const OutHomePresenter:React.SFC<IProps> = ({}) => 
+;
+
+export default OutHomePresenter;
